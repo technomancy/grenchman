731371f8b85cdd7a92d8fd56b3c335d1354bd9af
Phil Hagelberg <technomancy@gmail.com>
2013-08-23 20:07:40 -0700

    Gracefully handle messages that arrive in chunks.
    Track leftover contents from parsing as well as partial messages to
    push back into the buffer for the following read.

--- grench.ml
+++ grench.ml
@@ -13,30 +13,41 @@ let main_form = sprintf "(binding [leiningen.core.main/*cwd* \"%s\"
                                  (when-not (and (number? c) (zero? c))
                                    (throw e))))))"
 
-let message_for cwd args =
+let message_for root cwd args =
   match Uuid.sexp_of_t (Uuid.create ()) with
       Sexp.Atom uuid -> [("op", Bencode.String("eval"));
                          ("id", Bencode.String(uuid));
                          ("ns", Bencode.String("user"));
-                         ("code", Bencode.String(main_form cwd
+                         ("code", Bencode.String(main_form root cwd
                                                    (args_vector args)))]
     | Sexp.List _ -> [] (* no. *)
 
-let println = printf "%s\n%!"
+let rec print_status = function
+  | Bencode.String(status) :: tl -> printf "Status: %s\n%!" status;
+    print_status tl
+  | x :: tl -> printf "unknown status: %s" (Bencode.marshal x)
+  | [] -> ()
 
 let handler raw resp =
   (* TODO: got to be a better way to do this over alists *)
   match List.Assoc.find resp "out" with
-    | Some Bencode.String(out) -> println out
-    | Some _ | None -> match List.Assoc.find resp "value" with
-        | Some Bencode.String(value) -> println value
-        | Some _ | None -> match List.Assoc.find resp "status" with
-            | Some Bencode.String(status) -> printf "Status: %s\n%!" status
-            | Some _ | None -> printf "unknown response: %s\n%!" raw
+    | Some Bencode.String(out) -> printf "%s%!" out
+    | Some _ | None -> match List.Assoc.find resp "err" with
+        | Some Bencode.String(out) -> eprintf "%s%!" out
+        | Some _ | None -> match List.Assoc.find resp "value" with
+            | Some Bencode.String(value) -> ()
+            | Some _ | None -> match List.Assoc.find resp "status" with
+                (* TODO: Async exit isn't unit; huh? *)
+                | Some Bencode.List([Bencode.String("done")]) -> exit 0; ()
+                | Some Bencode.List([Bencode.String("eval-error")]) -> exit 1; ()
+                | Some Bencode.List(status) -> print_status status
+                | Some _ | None -> printf "unknown response: %s\n%!" raw
 
-let main cwd args =
-  Nrepl.send_and_receive "127.0.0.1" 50454
-    (message_for cwd args) handler
+let main cwd root args =
+  match Sys.getenv "LEIN_REPL_PORT" with
+    | Some port -> Nrepl.send_and_receive "127.0.0.1" (Int.of_string port)
+      (message_for cwd root args) handler
+    | None -> Printf.printf "%s\n%!" "Must set LEIN_REPL_PORT."; exit 1; ()
 
 let rec find_root cwd original =
   match Sys.file_exists (String.concat ~sep:Filename.dir_sep
@@ -47,14 +58,15 @@ let rec find_root cwd original =
       else
         find_root (Filename.dirname cwd) original
 
-(* TODO: this uses single-dash args; uuuugh *)
+(* TODO: this gets in the way of a bunch of useful lein flags *)
 let command =
   Command.basic
     ~summary:"Send commands to a running Leiningen instance"
     Command.Spec.(
       empty
       +> anon (sequence ("args" %: string)))
-    (fun args () -> main (find_root (Sys.getcwd ()) (Sys.getcwd ())) args)
+    (fun args () -> main (find_root (Sys.getcwd ()) (Sys.getcwd ()))
+      (Sys.getcwd ()) args)
 
 let () =
   Command.run ~version:"0.0.1" command;
--- nrepl.ml
+++ nrepl.ml
@@ -1,28 +1,52 @@
 open Async.Std
 open Core.Std
 
-let buffer_size = 1024
+let buffer_size = (1024 * 16)
 
 let send message w =
   let encoded = Bencode.marshal (Bencode.Dict(message)) in
   Writer.write w encoded
 
-(* TODO: handle case of reading too much into buffer at once *)
-let rec receive_until_done r handler buffer id close =
-  let parse_response = fun handler buffer id close resp ->
+
+let get_leftover buffer parsed bytes_read =
+  match parsed with
+    | Some parsed ->
+      let bytes_parsed = String.length (Bencode.marshal parsed) in
+      let length = bytes_read - bytes_parsed in
+      String.sub buffer bytes_parsed length
+    | None -> String.sub buffer 0 bytes_read
+
+let rec receive_until_done r handler buffer partial close =
+  let parse_single = fun raw bytes_read ->
+    let parsed = try Some (Bencode.parse raw) with
+      | _ -> None in
+    let leftover = get_leftover buffer parsed bytes_read in
+    (parsed, leftover) in
+
+  let rec handle_responses = fun handler raw bytes_read ->
+    match parse_single raw bytes_read with
+      | Some Bencode.Dict parsed, leftover -> handler raw parsed;
+        handle_responses handler leftover (bytes_read - (String.length (Bencode.marshal (Bencode.Dict parsed))))
+      | Some parsed, leftover -> Printf.printf "Unexpected response: %s %s\n%!"
+        (Bencode.string_of_type parsed) (Bencode.marshal parsed);
+        handle_responses handler leftover bytes_read
+      | None, leftover -> leftover in
+
+  let parse_response = fun handler buffer close resp ->
     match resp with
       | `Eof -> close ()
-      | `Ok length -> let response_str = String.sub buffer 0 length in
-                      handler response_str (Bencode.to_dict (Bencode.parse response_str));
-                      receive_until_done r handler buffer id close in
-  Reader.read r buffer >>= parse_response handler buffer id close
+      | `Ok bytes_read -> let raw = String.sub buffer 0 bytes_read in
+                          let partial =
+                            handle_responses handler raw bytes_read in
+                          receive_until_done r handler buffer partial close in
 
+  String.blit partial 0 buffer 0 (String.length partial);
+  Reader.read r buffer >>= parse_response handler buffer close
 
 let send_and_receive host port message handler =
   let _ = Tcp.connect (Tcp.to_host_and_port host port)
           >>= (fun (_, r, w) ->
             send message w;
             receive_until_done r handler
-              (String.create buffer_size)
-              (List.Assoc.find message "id")
+              (String.create buffer_size) ""
               (fun () -> Reader.close r)) in ()
