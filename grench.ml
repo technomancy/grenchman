23f538ee35791c1ae0b3ebed9dac7b526ce41dbb
Phil Hagelberg <technomancy@gmail.com>
2013-08-26 22:05:13 -0700

    Move repl-specific bits to Repl module.

--- grench.ml
+++ grench.ml
@@ -2,8 +2,6 @@ open Async.Std
 open Core.Std
 open Printf
 
-let exit = Pervasives.exit
-
 let splice_args args =
   String.concat ~sep:"\" \"" (List.map args String.escaped)
 
@@ -23,76 +21,20 @@ let main_form = sprintf "(binding [*cwd* \"%s\", *exit-process?* false]
                                  (when-not (and (number? c) (zero? c))
                                    (throw e))))))"
 
-let eval_message root cwd args session =
+let main_message root cwd args session =
   [("session", session);
    ("op", "eval");
-   ("id", "eval-" ^ (Uuid.to_string (Uuid.create ())));
+   ("id", Uuid.to_string (Uuid.create ()));
    ("ns", "leiningen.core.main");
    ("code", main_form root cwd (splice_args args))]
-
-let stdin_message input session =
-  let uuid = Uuid.to_string (Uuid.create ()) in
-  [("op", "stdin");
-   ("id", uuid);
-   ("stdin", input ^ "\n");
-   ("session", session)]
-
-let send_input resp (r,w,p) result =
-  match List.Assoc.find resp "session" with
-    | Some Bencode.String(session) -> let input = match result with
-        | `Ok input -> input
-        | `Eof -> "" in
-                      let message = stdin_message input session in
-                      Nrepl.send w p message
-    | None | Some _ -> eprintf "  No session in need-input."
-
-let rec handler (r,w,p) raw resp =
-  let handle k v = match (k, v) with
-    | ("out", out) -> printf "%s%!" out
-    | ("err", out) -> eprintf "%s%!" out
-    | ("ex", out) | ("root-ex", out) -> eprintf "%s\n%!" out
-    | ("value", value) -> Nrepl.debug ("-: " ^ value)
-    | ("session", _) | ("id", _) | ("ns", _) -> ()
-    | (k, v) -> printf "  Unknown response: %s %s\n%!" k v in
-
-  let remove_pending pending id =
-    Nrepl.debug ("-p " ^ String.concat ~sep:" " (! pending));
-    match id with
-      | Some Bencode.String(id) -> if List.mem (! pending) id then
-          pending := List.filter (! pending) ((<>) id)
-      | None | Some _ -> eprintf "  Unknown message id.\n%!" in
-
-  let handle_done resp pending =
-    remove_pending pending (List.Assoc.find resp "id");
-    if ! pending = ["init"] then exit 0 in
-
-  let rec handle_status resp status =
-    match status with
-      (* TODO: handle messages with multiple status fields by recuring on tl *)
-      | Bencode.String("done") :: tl -> handle_done resp p
-      | Bencode.String("eval-error") :: tl -> exit 1
-      | Bencode.String("unknown-session") :: tl -> eprintf "Unknown session.\n"; exit 1
-      | Bencode.String("need-input") :: tl -> let stdin = Async_unix.Fd.stdin () in
-                              let rdr = Reader.create stdin in
-                              Reader.read_line rdr >>| send_input resp (r,w,p); ()
-      | x -> printf "  Unknown status: %s\n%!" (Bencode.marshal (Bencode.List(x))) in
-
-  (* currently if it's a status message we ignore every other field *)
-  match List.Assoc.find resp "status" with
-    | Some Bencode.List(status) -> handle_status resp status
-    | Some _ -> eprintf "  Unexpected status type: %s\n%!" raw
-    | None -> match resp with
-        | (k, Bencode.String(v)) :: tl -> handle k v; handler (r,w,p) raw tl
-        | _ :: tl -> printf "  Unknown response: %s\n%!" raw; handler (r,w,p) raw tl
-        | [] -> ()
 
 let port_err () =
   eprintf "Couldn't read port from ~/.lein/repl-port or LEIN_REPL_PORT.\n
 If Leiningen is not running, launch `lein repl :headless' from outside a
 project directory and try again.\n";
-  exit 1
+  Pervasives.exit 1
 
-let repl_port root =
+let repl_port () =
   match Sys.getenv "LEIN_REPL_PORT" with
     | Some port -> port
     | None -> let filename = String.concat
@@ -103,9 +45,9 @@ let repl_port root =
                 | `No | `Unknown -> port_err ()
 
 let main cwd root args =
-  let port = Int.of_string (repl_port root) in
-  let message = eval_message cwd root args in
-  Nrepl.new_session "127.0.0.1" port message handler
+  let port = Int.of_string (repl_port ()) in
+  let message = main_message cwd root args in
+  Nrepl.new_session "127.0.0.1" port message Repl.handler
 
 let rec find_root cwd original =
   match Sys.file_exists (String.concat ~sep:Filename.dir_sep
---
+++ repl.ml
@@ -0,0 +1,69 @@
+open Async.Std
+open Core.Std
+open Printf
+
+let exit = Pervasives.exit
+
+let rdr = Reader.create (Async_unix.Fd.stdin ())
+
+let eval_message code session =
+  [("session", session);
+   ("op", "eval");
+   ("id", "eval-" ^ (Uuid.to_string (Uuid.create ())));
+   ("ns", "user");
+   ("code", code ^ "\n")]
+
+let stdin_message input session =
+  let uuid = Uuid.to_string (Uuid.create ()) in
+  [("op", "stdin");
+   ("id", uuid);
+   ("stdin", input ^ "\n");
+   ("session", session)]
+
+let send_input resp (r,w,p) result =
+  match List.Assoc.find resp "session" with
+    | Some Bencode.String(session) -> let input = match result with
+        | `Ok input -> input
+        | `Eof -> "" in
+                      let message = stdin_message input session in
+                      Nrepl.send w p message
+    | None | Some _ -> eprintf "  No session in need-input."
+
+let rec handler (r,w,p) raw resp =
+  let handle k v = match (k, v) with
+    | ("out", out) -> printf "%s%!" out
+    | ("err", out) -> eprintf "%s%!" out
+    | ("ex", out) | ("root-ex", out) -> eprintf "%s\n%!" out
+    | ("value", value) -> printf "%s\n%!" value
+    | ("session", _) | ("id", _) | ("ns", _) -> ()
+    | (k, v) -> printf "  Unknown response: %s %s\n%!" k v in
+
+  let remove_pending pending id =
+    Nrepl.debug ("-p " ^ String.concat ~sep:" " (! pending));
+    match id with
+      | Some Bencode.String(id) -> if List.mem (! pending) id then
+          pending := List.filter (! pending) ((<>) id)
+      | None | Some _ -> eprintf "  Unknown message id.\n%!" in
+
+  let handle_done resp pending =
+    remove_pending pending (List.Assoc.find resp "id");
+    if ! pending = ["init"] then exit 0 in
+
+  let rec handle_status resp status =
+    match status with
+      (* TODO: handle messages with multiple status fields by recuring on tl *)
+      | Bencode.String("done") :: tl -> handle_done resp p
+      | Bencode.String("eval-error") :: tl -> exit 1
+      | Bencode.String("unknown-session") :: tl -> eprintf "Unknown session.\n"; exit 1
+      | Bencode.String("need-input") :: tl -> Reader.read_line rdr >>|
+          send_input resp (r,w,p); ()
+      | x -> printf "  Unknown status: %s\n%!" (Bencode.marshal (Bencode.List(x))) in
+
+  (* currently if it's a status message we ignore every other field *)
+  match List.Assoc.find resp "status" with
+    | Some Bencode.List(status) -> handle_status resp status
+    | Some _ -> eprintf "  Unexpected status type: %s\n%!" raw
+    | None -> match resp with
+        | (k, Bencode.String(v)) :: tl -> handle k v; handler (r,w,p) raw tl
+        | _ :: tl -> printf "  Unknown response: %s\n%!" raw; handler (r,w,p) raw tl
+        | [] -> ()
