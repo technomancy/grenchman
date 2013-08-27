bdc5ed4dfc4c8e3304b7aff3c15c39a3355b4d08
Phil Hagelberg <technomancy@gmail.com>
2013-08-26 19:06:12 -0700

    Initiate a session properly and use it for all messages.

--- grench.ml
+++ grench.ml
@@ -2,6 +2,8 @@ open Async.Std
 open Core.Std
 open Printf
 
+let exit = Pervasives.exit
+
 let splice_args args =
   String.concat ~sep:"\" \"" (List.map args String.escaped)
 
@@ -21,42 +23,55 @@ let main_form = sprintf "(binding [*cwd* \"%s\", *exit-process?* false]
                                  (when-not (and (number? c) (zero? c))
                                    (throw e))))))"
 
-let message_for root cwd args =
+let eval_message root cwd args session =
+  (* TODO: move id generation to Nrepl.send *)
   match Uuid.sexp_of_t (Uuid.create ()) with
-      Sexp.Atom uuid -> [("op", Bencode.String("eval"));
-                         ("id", Bencode.String(uuid));
+      Sexp.Atom uuid -> [("session", Bencode.String(session));
+                         ("op", Bencode.String("eval"));
+                         ("id", Bencode.String("eval-" ^ uuid));
                          ("ns", Bencode.String("leiningen.core.main"));
                          ("code", Bencode.String(main_form root cwd
                                                    (splice_args args)))]
     | Sexp.List _ -> [] (* no. *)
 
-let rec handle_status = function
-  | Bencode.String("done") :: tl -> exit 0; ()
-  | Bencode.String("eval-error") :: tl -> exit 0; ()
-  | Bencode.String(status) :: tl -> printf "Status: %s\n%!" status;
-    handle_status tl
-  | x :: tl -> printf "  Unknown status: %s\n%!" (Bencode.marshal x)
-  | [] -> ()
-
-let rec handler raw resp =
+let rec handler rw raw resp =
   let handle k v = match (k, v) with
     | ("out", out) -> printf "%s%!" out
     | ("err", out) -> eprintf "%s%!" out
     | ("ex", out) | ("root-ex", out) -> eprintf "%s\n%!" out
-    | ("value", value) -> (* printf "%s\n%!" value *) ()
+    | ("value", value) -> Nrepl.debug ("-: " ^ value)
     | ("session", _) | ("id", _) | ("ns", _) -> ()
     | (k, v) -> printf "  Unknown response: %s %s\n%!" k v in
-  match resp with
-    | (k, Bencode.String v) :: tl -> handle k v; handler raw tl
-    | ("status", Bencode.List s) :: tl -> handle_status s; handler raw tl
-    | (_, _) :: tl -> printf "  Unknown response: %s\n%!" raw; handler raw tl
-    | [] -> ()
+
+  let handle_done resp =
+    match List.Assoc.find resp "id" with
+      (* TODO: terrible heuristic; need to track ids properly *)
+      | Some Bencode.String(id) -> if String.sub id 0 5 = "eval-" then
+          exit 0 else ()
+      | None | Some _ -> () in
+
+  let rec handle_status resp status =
+    match status with
+      (* TODO: handle messages with multiple status fields by recuring on tl *)
+      | Bencode.String("done") :: tl -> handle_done resp
+      | Bencode.String("eval-error") :: tl -> exit 1
+      | Bencode.String("unknown-session") :: tl -> eprintf "Unknown session.\n"; exit 1
+      | x -> printf "  Unknown status: %s\n%!" (Bencode.marshal (Bencode.List(x))) in
+
+  (* currently if it's a status message we ignore every other field *)
+  match List.Assoc.find resp "status" with
+    | Some Bencode.List(status) -> handle_status resp status
+    | Some _ -> eprintf "  Unexpected status type: %s\n%!" raw
+    | None -> match resp with
+        | (k, Bencode.String(v)) :: tl -> handle k v; handler rw raw tl
+        | _ :: tl -> printf "  Unknown response: %s\n%!" raw; handler rw raw tl
+        | [] -> ()
 
 let port_err () =
   eprintf "Couldn't read port from ~/.lein/repl-port or LEIN_REPL_PORT.\n
 If Leiningen is not running, launch `lein repl :headless' from outside a
 project directory and try again.\n";
-  exit 1; "-1"
+  exit 1
 
 let repl_port root =
   match Sys.getenv "LEIN_REPL_PORT" with
@@ -70,8 +85,8 @@ let repl_port root =
 
 let main cwd root args =
   let port = Int.of_string (repl_port root) in
-  let message = message_for cwd root args in
-  Nrepl.send_and_receive "127.0.0.1" port message handler
+  let message = eval_message cwd root args in
+  Nrepl.new_session "127.0.0.1" port message handler
 
 let rec find_root cwd original =
   match Sys.file_exists (String.concat ~sep:Filename.dir_sep
@@ -88,9 +103,10 @@ A replacement launcher for running Leiningen tasks.
 See `grench help' to list tasks."
 
 let () =
-  let cwd = Sys.getcwd () in
-  let root = find_root cwd cwd in
-  match Sys.argv |> Array.to_list |> List.tl with
-    | None | Some ["--grench-help"] -> printf "%s\n%!" usage
-    | Some args -> main root cwd args;
-  never_returns (Scheduler.go ())
+  if ! Sys.interactive then () else
+    let cwd = Sys.getcwd () in
+    let root = find_root cwd cwd in
+    match Sys.argv |> Array.to_list |> List.tl with
+      | None | Some ["--grench-help"] -> printf "%s\n%!" usage
+      | Some args -> main root cwd args;
+        never_returns (Scheduler.go ())
--- nrepl.ml
+++ nrepl.ml
@@ -3,8 +3,15 @@ open Core.Std
 
 let buffer_size = (1024 * 16)
 
-let send message w =
-  Bencode.Dict message |> Bencode.marshal |> Writer.write w
+let debug out =
+  match Sys.getenv "DEBUG" with
+    | Some _ -> Printf.printf "%s\n%!" out
+    | None -> ()
+
+let send w message =
+  let out = Bencode.marshal (Bencode.Dict message) in
+  debug ("-> " ^ out);
+  Writer.write w out
 
 let get_leftover buffer parsed bytes_read =
   match parsed with
@@ -14,39 +21,53 @@ let get_leftover buffer parsed bytes_read =
       String.sub buffer bytes_parsed length
     | None -> String.sub buffer 0 bytes_read
 
-let rec receive_until_done r handler buffer partial close =
-  let parse_single = fun raw bytes_read ->
+let rec receive_until_done (r,w) handler buffer partial =
+  let parse_single raw bytes_read =
     let parsed = try Some (Bencode.parse raw) with
       | _ -> None in
     let leftover = get_leftover buffer parsed bytes_read in
     (parsed, leftover) in
 
-  let rec handle_responses = fun handler raw bytes_read ->
+  let rec handle_responses handler raw bytes_read =
     match parse_single raw bytes_read with
-      | Some Bencode.Dict parsed, leftover -> handler raw parsed;
+      | Some Bencode.Dict parsed, leftover -> handler (r,w) raw parsed;
         let re_encoded = (Bencode.marshal (Bencode.Dict parsed)) in
         let bytes_parsed = String.length re_encoded in
+        debug ("<- " ^ re_encoded);
         handle_responses handler leftover (bytes_read - bytes_parsed)
-      | Some parsed, leftover -> Printf.printf "Unexpected %s: %s\n%!"
-        (Bencode.string_of_type parsed) (Bencode.marshal parsed);
+      | Some parsed, leftover -> Printf.printf "Unexpected %s: %s | %s \n%!"
+        (Bencode.string_of_type parsed) (Bencode.marshal parsed) leftover;
         handle_responses handler leftover bytes_read
       | None, leftover -> leftover in
 
-  let parse_response = fun handler buffer close resp ->
+  let parse_response handler buffer resp =
     match resp with
-      | `Eof -> close ()
+      | `Eof -> Reader.close r
       | `Ok bytes_read -> let raw = String.sub buffer 0 bytes_read in
                           let partial =
                             handle_responses handler raw bytes_read in
-                          receive_until_done r handler buffer partial close in
+                          receive_until_done (r,w) handler buffer partial in
 
   String.blit partial 0 buffer 0 (String.length partial);
-  Reader.read r buffer >>= parse_response handler buffer close
+  Reader.read r buffer >>= parse_response handler buffer
 
-let send_and_receive host port message handler =
-  let _ = Tcp.connect (Tcp.to_host_and_port host port)
-          >>= (fun (_, r, w) ->
-            send message w;
-            receive_until_done r handler
-              (String.create buffer_size) ""
-              (fun () -> Reader.close r)) in ()
+let get_session buffer resp =
+  let no_session ()  = Printf.eprintf "No session!"; Pervasives.exit 0 in
+  match resp with
+    | `Eof -> no_session ()
+    | `Ok bytes_read -> match Bencode.parse (String.sub buffer 0 bytes_read) with
+        | Bencode.Int _ | Bencode.String _ | Bencode.List _ -> no_session ()
+        | Bencode.Dict(d) -> match List.Assoc.find d "new-session" with
+            | Some Bencode.String(session) -> session
+            | Some _ | None -> no_session ()
+
+let initiate (r, w) buffer handler message resp =
+  get_session buffer resp |> message |> send w;
+  receive_until_done (r, w) handler buffer ""
+
+let new_session host port message handler =
+  let buffer = (String.create buffer_size) in
+  Tcp.connect (Tcp.to_host_and_port host port)
+  >>= (fun (_, r, w) ->
+    send w [("op", Bencode.String("clone"))];
+    Reader.read r buffer >>= initiate (r, w) buffer handler message)
