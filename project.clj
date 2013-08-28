7c342c2f12f11928ced174140405f75fee0038d6
Phil Hagelberg <technomancy@gmail.com>
2013-08-27 21:07:11 -0700

    Remove dummy lein project.

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject dummy "0.0.0."
-  :description "Simply for testing."
-  :eval-in :leiningen
-  :repl-options {:port 50454
-                 :nrepl-middleware [(fn [handler]
-                                      (fn [& args]
-                                        (prn :middle args)
-                                        (apply handler args)))]})
