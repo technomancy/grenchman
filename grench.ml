cfdd43cfb8e636f709687f4e6cd476bb80b6bbd6
Phil Hagelberg <technomancy@gmail.com>
2013-10-01 18:25:37 -0700

    Move lein task invocation to subcommand.

--- grench.ml
+++ grench.ml
@@ -3,12 +3,16 @@ open Printf
 
 (* Entry point module; handles command-line args and dispatching. *)
 
-let usage = "usage: grench TASK [ARGS]...
+let usage = "Grenchman runs Clojure code quickly.
 
-Run Clojure code in nREPL servers."
+Commands:
 
-let port_err =
-  "Couldn't read port from .nrepl-port or GRENCH_PORT.\n"
+  main NAMESPACE[/FUNCTION] [ARGS...]   Run an existing project function.
+  eval FORM                             Eval form inside project.
+  repl [:connect PORT]                  Connect a repl.
+  lein [TASK ARGS...]                   Run a Leiningen task.\n\n"
+
+let port_err = "Couldn't read port from .nrepl-port or $GRENCH_PORT.\n"
 
 let repl_port port_file =
   let cwd = Sys.getcwd () in
@@ -25,10 +29,16 @@ let () =
     match Sys.argv |> Array.to_list |> List.tl with
       | None | Some ["--grench-help"] -> printf "%s\n%!" usage
       | Some ["--version"] | Some ["-v"] -> printf "Grenchman 0.1.0\n%!"
-      | Some ("main" :: args) -> Client.main (repl_port ".nrepl-port") args
       | Some ["--leiningen-version"] | Some ["--lein-version"] ->
         Lein.main ["version"]
-      | Some ["raw-repl"] -> Lein.main ["run"; "-m"; "clojure.main/main"; "-r"]
+
+      | Some ("main" :: args) -> Client.main (repl_port ".nrepl-port") args
+      | Some ("eval" :: args) ->
+        Client.main (repl_port ".nrepl-port")
+          ("clojure.main/main" :: "-e" :: args)
+
       | Some ["repl"] -> Repl.main (repl_port ".nrepl-port")
       | Some ["repl"; ":connect"; port] -> Repl.main (int_of_string port)
-      | Some args -> Lein.main args
+      | Some ("lein" :: args) -> Lein.main args
+      | Some _ -> eprintf "Unknown command.\n\n%s" usage; exit 1
+
